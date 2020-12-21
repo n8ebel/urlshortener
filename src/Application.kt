4a203f832c4301d0b3de1f1642b0b817b03c6b06
Nate Ebel <n8ebel@goobar.io>
2020-12-20 20:47:11 -0800

    Create project

---
+++ src/Application.kt
@@ -0,0 +1,20 @@
+package com.goobar
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("We're going to shorten some URLs!!", contentType = ContentType.Text.Plain)
+        }
+    }
+}
+
